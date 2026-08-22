3f93e668d0f47483f8852cf940cdb2d73209416b
ilnar-esanov <ilnaresanov2002@gmail.com>
2026-08-22 23:00:04 +0500

    tasks 5-11

--- script/homework-9.js
+++ script/homework-9.js
@@ -1,14 +1,61 @@
-// task #1 - filter array with numbers
+// task #2 - Создать массив чисел от 1 до 10. Отфильтровать его таким образом, что бы мы получил массив чисел, начиная с 5.
 
 const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-const filteredNUmbers = numbers.filter((number) => number >= 5);
-console.log(filteredNUmbers);
+const filteredNumbers = numbers.filter((number) => number >= 5);
+// console.log(filteredNumbers);
 
-// task #2 - includes method
+// task #3 - Создать массив строк, относящихся к любой сущности (название фильмов/книг, кухонные приборы, мебель и т.д.), проверить, есть ли в массиве какая-то определенная сущность.
+
 const fruits = ['apple', 'pear', 'cherry', 'watermelon'];
-console.log(fruits.includes('apple'));
+// console.log(fruits.includes('apple'));
 
-// task #3 - reverse array function 
+// task #4 - Написать функцию, которая аргументом будет принимать массив и изменять его порядок на противоположный ("переворачивать") . Два вышеуказанных массива с помощью этой функции перевернуть.
+
 const reverseArray = (array1) => array1.reverse();
-console.log(reverseArray(numbers));
-console.log(reverseArray(fruits));
+// console.log(reverseArray(numbers));
+// console.log(reverseArray(fruits));
+
+// task #5 => in the file comments.js
+
+// task #6 - import variable
+import { comments } from './comments.js';
+
+// task #7 - Вывести в консоль массив тех комментариев, почта пользователей которых содержит ".com"
+
+// console.log(comments.filter((comment) => comment.email.includes('.com')));
+
+// task #8 - Перебрать массив таким образом, что бы пользователи с id меньше или равно 5 имели postId: 2, а те, у кого id больше 5, имели postId: 1
+
+const filteredComments = comments.map((comment) => ({
+  ...comment,
+  postId: comment.id <= 5 ? 2 : 1,
+}));
+// console.log(filteredComments);
+
+// task #9 - Перебрать массив, что бы объекты состояли только из айди и имени
+
+const updatedComments = comments.map((comment) => ({
+  id: comment.id,
+  name: comment.name,
+}));
+// console.log(updatedComments);
+
+// task #10 - Перебираем массив, добавляем объектам свойство isInvalid и проверяем: если длина тела сообщения (body) больше 180 символов - устанавливаем true, меньше - false.
+
+const checkedComments = comments.map((comment) => ({
+  ...comment,
+  isInvalid: comment.body.length > 180,
+}));
+// console.log(checkedComments);
+
+// task #11 - Почитать про метод массива reduce. Используя его, вывести массив почт и провернуть тоже самое с помощью метода map
+
+const emailsReduce = comments.reduce((accumulator, comment) => {
+  accumulator.push(comment.email);
+  return accumulator;
+}, []);
+
+console.log(emailsReduce); 
+
+const commentsWithEmails = comments.map((comment) => comment.email);
+console.log(commentsWithEmails);
