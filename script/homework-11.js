70f9037d1ad0860693b20f111b4127f77d787ede
ilnar-esanov <ilnaresanov2002@gmail.com>
2026-09-05 23:45:04 +0500

    добавил открытие и закрытие формы

--- script/homework-11.js
+++ script/homework-11.js
@@ -1,14 +1,18 @@
 const form = document.querySelector('.form');
 const input = document.querySelector('.form__input');
 
+const signUpButton = document.querySelector('.form__sign-up');
+const formBlock = document.querySelector('.modal');
+const signUpForm = document.querySelector('.modal__form');
+const closeButton = document.querySelector('.modal__close');
+
 
 // способ №1
 form.addEventListener('submit', (event) => {
   event.preventDefault();
-  const Inputemail = input.value
-  console.log({email: Inputemail});
-})
-
+  const Inputemail = input.value;
+  console.log({ email: Inputemail });
+});
 
 // способ №2
 form.addEventListener('submit', (event) => {
@@ -18,4 +22,12 @@ form.addEventListener('submit', (event) => {
   const userEmail = formData.get('email');
 
   console.log({ email: userEmail });
+});
+
+signUpButton.addEventListener('click', () => {
+  formBlock.classList.add('modal-showed');
+});
+
+closeButton.addEventListener('click', () => {
+  formBlock.classList.remove('modal-showed');
 });
