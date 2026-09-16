c9dc90859017892dd1821c6fb454ad14f196b256
ilnar-esanov <ilnaresanov2002@gmail.com>
2026-09-16 15:39:43 +0500

    добавил сброс для первой формы

--- script/homework-11.js
+++ script/homework-11.js
@@ -2,9 +2,20 @@ import { Modal } from './Modal.js';
 import { Form } from './Form.js';
 
 const signUpButton = document.querySelector('.form__sign-up');
+const emailForm = document.querySelector('.form')
 
 const modalWindow = new Modal('modal');
 const formBlock = new Form('form');
+
+emailForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const formData = new FormData(emailForm);
+  const userEmail = formData.get('email');
+
+  console.log({ email: userEmail });
+  emailForm.reset();
+});
 
 signUpButton.addEventListener('click', () => modalWindow.openModal());
 
--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 import './script.js';
-// import './homework-7.js';
-// import './homework-8.js';
-// import './homework-9.js';
+import './homework-7.js';
+import './homework-8.js';
+import './homework-9.js';
 import './homework-10.js';
 import './homework-11.js';
 import './homework-12.js';
